cd9b7661e518d768a7895e533acdfc4ae29da6b0
Sadra536 <fatmhrwzbhy99@gmail.com>
2026-08-08 19:35:09 +0330

    Update check_site.py

--- check_site.py
+++ check_site.py
@@ -74,7 +74,7 @@ def main():
     human_delay(0.5, 2)
 
     # هر کاربر چند بار صفحه رو باز می‌کنه (شبیه رفرش/گشتن)
-    visits = random.randint(10000,20000 )
+    visits = random.randint(500,800 )
     for _ in range(visits):
         page = random.choice(PAGES)
         check_page(page)
